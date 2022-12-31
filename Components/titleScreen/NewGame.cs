76cf7a382eb495190aa56fa7e084218422458da3
Hiroji <joegilbertdev@outlook.com>
2022-12-31 13:40:49 +0000

    NewGame- saves soceity choice as field

--- Components/titleScreen/NewGame.cs
+++ Components/titleScreen/NewGame.cs
@@ -2,6 +2,7 @@ using Spectre.Console;
 
 class NewGame
 {
+    private string? soceity;
 
     //methods
     public void createNewGame()
@@ -10,20 +11,36 @@ class NewGame
         Hello, you are spark from which trillions will emerge,
         In order to start your journey we will need to ask you some questions.");
 
+
+
         //console choices
-        string homeStar = AnsiConsole.Prompt(
+        string soceityChoice = AnsiConsole.Prompt(
         new SelectionPrompt<string>()
             .Title("Please select your [green]home star system:[/]")
             .PageSize(10)
             .AddChoices(new[] {
-            "Sol", "Vilous"
+            "The Re-United Nations of Earth", "The Shigu Imperium", "The Enlightened Kingdom of Gold Ring"
             }));
 
+        //convert text into var name
+        switch (soceityChoice)
+        {
+            case "The Re-United Nations of Earth":
+                this.soceity = soceityChoice;
+                break;
+            case "The Shigu Imperium":
+                this.soceity = soceityChoice;
+                break;
+            case "The Enlightened Kingdom of Gold Ring":
+            this.soceity = soceityChoice;
+            break;
+        }
+
         // Echo 
-        Console.WriteLine($"Your soceity was founded in the {homeStar} system.");
+        Console.WriteLine($"You choose to play as {this.soceity}.");
         //add civ bios
         //recrd new game to json
-        
+
 
 
 
--- README.md
+++ README.md
@@ -7,9 +7,13 @@ In VS Code: install dotNet and run in terminal `dotnet run`.
 
 ## Game Mechanics
 
-### 1. Civilization setup (select starting world) / UN / vilous -> print char -> set generators etc
+### 1. Civilization setup (select starting world) set generators etc
+### 1.1 Starting civilisation choices
+ - The Re-United Nations of Earth:
+ - The Shigu Imperium:
+ - The Enlightened Kingdom of Gold Ring:
 
-### 1.1. Map (start planet -> solar system -> neighbouring systems etc).
+### 1.2. Map (start planet -> solar system -> neighbouring systems etc).
 
 ### 2. Mining (bring back excess fuel, minerals, water).
 
